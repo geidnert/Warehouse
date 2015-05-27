1232174f59b6b96fe7efb4ec85e103c93c4abb8c
Andreas Geidnert <geidnert@gmail.com>
2015-05-27 08:56:42 +0200

    Transition to add activity from main activity

--- app/src/main/java/com/solidparts/warehouse/MainActivity.java
+++ app/src/main/java/com/solidparts/warehouse/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -34,5 +35,9 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onAddItem(View view) {
+        setContentView(R.layout.activity_add_item);
     }
 }
