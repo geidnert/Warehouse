f1a48d9f42e255eb2be1c881a52effa852f485f3
Andreas Geidnert <geidnert@gmail.com>
2015-06-25 14:39:15 +0200

    Changed to live settings for release

--- app/src/main/java/com/solidparts/warehouse/MainActivity.java
+++ app/src/main/java/com/solidparts/warehouse/MainActivity.java
@@ -31,7 +31,7 @@ import java.io.IOException;
 
 
 public class MainActivity extends FragmentActivity {
-    public static final int APP_VERSION = 2;
+    public static final int APP_VERSION = 3;
     public static final int IMAGE_GALLERY_REQUEST = 1;
 
     private ItemService itemService;
